4d77d0f2f298065a26638d5d6a8fc89a4d4f3610
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 04:43:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,15 +9,16 @@ class CashRegister
     @discount = discount
     @total = 0
   end
-  
+
   def add_item(title,price,quantity=1)
-    
+
     while quantity>0
       quantity-=1
       @@items << title
+    end
     self.total+= quantity*price
   end
-  
+
   def apply_discount
     self.total = self.total - (self.discount*self.discount)/100
     self.discount==0?"There is no discount to apply.":"After the discount, the total comes to #{self.total}."
