0cc866fd19654ea655cdee3429cb7f6cab366f07
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 04:54:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     self.total+= quantity*price
     while quantity>0
       quantity-=1
-      @@items << title
+      @items << title
     end
   end
 
@@ -25,9 +25,7 @@ class CashRegister
     self.discount==0?"There is no discount to apply.":"After the discount, the total comes to $#{self.total.to_i}."
   end
 
-  def items
-    @@items
-  end
+
   def void_last_transaction
     total -= quantity*price
     arr.pop(quantity)
